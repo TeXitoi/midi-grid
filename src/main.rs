698d808ae6dbf24be611b8d0bbfd833436df98bd
Guillaume Pinot <texitoi@texitoi.eu>
2024-01-02 13:27:15 +0100

    update rtic and hal

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 #![no_main]
 #![no_std]
 
-use embedded_hal::digital::v2::OutputPin;
 use keyberon::debounce::Debouncer;
 use keyberon::layout::Event;
 use keyberon::matrix::Matrix;
 use panic_halt as _;
 use rtic::app;
-use stm32f1xx_hal::gpio::{Input, Output, PullUp, PushPull, Pxx};
+use stm32f1xx_hal::gpio::{EPin, Input, Output, PullUp, PushPull};
 use stm32f1xx_hal::prelude::*;
-use stm32f1xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
+use stm32f1xx_hal::usb::{self, Peripheral, UsbBus, UsbBusType};
 use stm32f1xx_hal::{pac, timer};
 use usb_device::bus::UsbBusAllocator;
 use usb_device::class::UsbClass as _;
@@ -27,47 +26,49 @@ type UsbClass = MidiClass<'static, UsbBusType>;
 type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
-const APP: () = {
-    struct Resources {
+mod app {
+    use super::*;
+
+    #[shared]
+    struct Shared {
         usb_dev: UsbDevice,
         usb_class: UsbClass,
-        matrix: Matrix<Pxx<Input<PullUp>>, Pxx<Output<PushPull>>, 12, 5>,
+    }
+    #[local]
+    struct Local {
+        matrix: Matrix<EPin<Input<PullUp>>, EPin<Output<PushPull>>, 12, 5>,
         debouncer: Debouncer<[[bool; 12]; 5]>,
-        timer: timer::CountDownTimer<pac::TIM3>,
+        timer: timer::CounterHz<pac::TIM3>,
     }
 
-    #[init]
-    fn init(mut c: init::Context) -> init::LateResources {
-        static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
-
+    #[init(local = [bus: Option<UsbBusAllocator<usb::UsbBusType>> = None])]
+    fn init(mut c: init::Context) -> (Shared, Local, init::Monotonics) {
         let mut flash = c.device.FLASH.constrain();
-        let mut rcc = c.device.RCC.constrain();
+        let rcc = c.device.RCC.constrain();
 
         // set 0x424C in DR10 for dfu on reset
-        let bkp = rcc
-            .bkp
-            .constrain(c.device.BKP, &mut rcc.apb1, &mut c.device.PWR);
+        let bkp = rcc.bkp.constrain(c.device.BKP, &mut c.device.PWR);
         bkp.write_data_register_low(9, 0x424C);
 
         let clocks = rcc
             .cfgr
-            .use_hse(8.mhz())
-            .sysclk(72.mhz())
-            .pclk1(36.mhz())
+            .use_hse(8.MHz())
+            .sysclk(72.MHz())
+            .pclk1(36.MHz())
             .freeze(&mut flash.acr);
 
-        let mut gpioa = c.device.GPIOA.split(&mut rcc.apb2);
-        let mut gpiob = c.device.GPIOB.split(&mut rcc.apb2);
-        let mut gpioc = c.device.GPIOC.split(&mut rcc.apb2);
+        let mut gpioa = c.device.GPIOA.split();
+        let mut gpiob = c.device.GPIOB.split();
+        let mut gpioc = c.device.GPIOC.split();
 
         // BluePill board has a pull-up resistor on the D+ line.
         // Pull the D+ pin down to send a RESET condition to the USB bus.
         let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-        usb_dp.set_low().unwrap();
-        cortex_m::asm::delay(clocks.sysclk().0 / 100);
+        usb_dp.set_low();
+        cortex_m::asm::delay(clocks.sysclk().raw() / 100);
 
         let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-        led.set_high().unwrap();
+        led.set_high();
 
         let usb_dm = gpioa.pa11;
         let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
@@ -78,8 +79,8 @@ const APP: () = {
             pin_dp: usb_dp,
         };
 
-        *USB_BUS = Some(UsbBus::new(usb));
-        let usb_bus = USB_BUS.as_ref().unwrap();
+        *c.local.bus = Some(UsbBus::new(usb));
+        let usb_bus = c.local.bus.as_ref().unwrap();
 
         let usb_class = MidiClass::new(&usb_bus, 1, 1).unwrap();
         let usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
@@ -87,71 +88,71 @@ const APP: () = {
             .device_class(USB_CLASS_NONE)
             .build();
 
-        let mut timer =
-            timer::Timer::tim3(c.device.TIM3, &clocks, &mut rcc.apb1).start_count_down(1.khz());
+        let mut timer = c.device.TIM3.counter_hz(&clocks);
+        timer.start(1.kHz()).unwrap();
         timer.listen(timer::Event::Update);
 
         let matrix = Matrix::new(
             [
-                gpiob.pb12.into_pull_up_input(&mut gpiob.crh).downgrade(),
-                gpiob.pb13.into_pull_up_input(&mut gpiob.crh).downgrade(),
-                gpiob.pb14.into_pull_up_input(&mut gpiob.crh).downgrade(),
-                gpiob.pb15.into_pull_up_input(&mut gpiob.crh).downgrade(),
-                gpioa.pa8.into_pull_up_input(&mut gpioa.crh).downgrade(),
-                gpioa.pa9.into_pull_up_input(&mut gpioa.crh).downgrade(),
-                gpioa.pa10.into_pull_up_input(&mut gpioa.crh).downgrade(),
-                gpiob.pb5.into_pull_up_input(&mut gpiob.crl).downgrade(),
-                gpiob.pb6.into_pull_up_input(&mut gpiob.crl).downgrade(),
-                gpiob.pb7.into_pull_up_input(&mut gpiob.crl).downgrade(),
-                gpiob.pb8.into_pull_up_input(&mut gpiob.crh).downgrade(),
-                gpiob.pb9.into_pull_up_input(&mut gpiob.crh).downgrade(),
+                gpiob.pb12.into_pull_up_input(&mut gpiob.crh).erase(),
+                gpiob.pb13.into_pull_up_input(&mut gpiob.crh).erase(),
+                gpiob.pb14.into_pull_up_input(&mut gpiob.crh).erase(),
+                gpiob.pb15.into_pull_up_input(&mut gpiob.crh).erase(),
+                gpioa.pa8.into_pull_up_input(&mut gpioa.crh).erase(),
+                gpioa.pa9.into_pull_up_input(&mut gpioa.crh).erase(),
+                gpioa.pa10.into_pull_up_input(&mut gpioa.crh).erase(),
+                gpiob.pb5.into_pull_up_input(&mut gpiob.crl).erase(),
+                gpiob.pb6.into_pull_up_input(&mut gpiob.crl).erase(),
+                gpiob.pb7.into_pull_up_input(&mut gpiob.crl).erase(),
+                gpiob.pb8.into_pull_up_input(&mut gpiob.crh).erase(),
+                gpiob.pb9.into_pull_up_input(&mut gpiob.crh).erase(),
             ],
             [
-                gpiob.pb11.into_push_pull_output(&mut gpiob.crh).downgrade(),
-                gpiob.pb10.into_push_pull_output(&mut gpiob.crh).downgrade(),
-                gpiob.pb1.into_push_pull_output(&mut gpiob.crl).downgrade(),
-                gpiob.pb0.into_push_pull_output(&mut gpiob.crl).downgrade(),
-                gpioa.pa7.into_push_pull_output(&mut gpioa.crl).downgrade(),
+                gpiob.pb11.into_push_pull_output(&mut gpiob.crh).erase(),
+                gpiob.pb10.into_push_pull_output(&mut gpiob.crh).erase(),
+                gpiob.pb1.into_push_pull_output(&mut gpiob.crl).erase(),
+                gpiob.pb0.into_push_pull_output(&mut gpiob.crl).erase(),
+                gpioa.pa7.into_push_pull_output(&mut gpioa.crl).erase(),
             ],
         );
 
-        init::LateResources {
-            usb_dev,
-            usb_class,
-            timer,
-            debouncer: Debouncer::new([[false; 12]; 5], [[false; 12]; 5], 5),
-            matrix: matrix.unwrap(),
-        }
+        (
+            Shared { usb_dev, usb_class },
+            Local {
+                timer,
+                debouncer: Debouncer::new([[false; 12]; 5], [[false; 12]; 5], 5),
+                matrix: matrix.unwrap(),
+            },
+            init::Monotonics(),
+        )
     }
 
-    #[task(binds = USB_HP_CAN_TX, priority = 2, resources = [usb_dev, usb_class])]
-    fn usb_tx(mut c: usb_tx::Context) {
-        usb_poll(&mut c.resources.usb_dev, &mut c.resources.usb_class);
+    #[task(binds = USB_HP_CAN_TX, priority = 2, shared = [usb_dev, usb_class])]
+    fn usb_tx(c: usb_tx::Context) {
+        (c.shared.usb_dev, c.shared.usb_class)
+            .lock(|usb_dev, usb_class| usb_poll(usb_dev, usb_class))
     }
 
-    #[task(binds = USB_LP_CAN_RX0, priority = 2, resources = [usb_dev, usb_class])]
-    fn usb_rx(mut c: usb_rx::Context) {
-        usb_poll(&mut c.resources.usb_dev, &mut c.resources.usb_class);
+    #[task(binds = USB_LP_CAN_RX0, priority = 2, shared = [usb_dev, usb_class])]
+    fn usb_rx(c: usb_rx::Context) {
+        (c.shared.usb_dev, c.shared.usb_class)
+            .lock(|usb_dev, usb_class| usb_poll(usb_dev, usb_class))
     }
 
-    #[task(binds = TIM3, priority = 1, resources = [usb_class, matrix, debouncer, timer])]
+    #[task(binds = TIM3, priority = 1, shared = [usb_class], local = [matrix, debouncer, timer])]
     fn tick(mut c: tick::Context) {
-        c.resources.timer.clear_update_interrupt_flag();
+        c.local.timer.clear_interrupt(timer::Event::Update);
 
-        for event in c
-            .resources
-            .debouncer
-            .events(c.resources.matrix.get().unwrap())
-        {
+        for event in c.local.debouncer.events(c.local.matrix.get().unwrap()) {
             while c
-                .resources
+                .shared
                 .usb_class
                 .lock(|midi| midi.send_message(midi_event(event)))
                 .is_err()
             {}
         }
     }
-};
+}
 
 fn midi_event(event: Event) -> UsbMidiEventPacket {
     let (y, x) = event.coord();
